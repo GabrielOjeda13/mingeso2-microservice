917e791684b6d8a2a5e4c1802e8cb75da2e71125
Gabriel Ojeda Romero <gabriel.ojeda@usach.cl>
2024-06-05 20:02:39 -0400

    configuración del config-data

--- ms-vehiculo/src/main/java/com/mingeso/msvehiculo/MsVehiculoApplication.java
+++ ms-vehiculo/src/main/java/com/mingeso/msvehiculo/MsVehiculoApplication.java
@@ -2,8 +2,10 @@ package com.mingeso.msvehiculo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsVehiculoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MsVehiculoApplication.class, args);
